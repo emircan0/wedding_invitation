496a1ac70f2122da9c1774d174362ad98a308543
Emircan Mert <emircanmertt@gmail.com>
2026-05-21 15:27:12 +0300

    feat: galeriden 'samimiyet' (whatsapp-2.jpg) kaldırıldı ve yerine img-20221115-205820-068.jpg eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ const GALLERY = [
   { src: '/photos/img-6310.jpg', alt: 'Soğuk havada selfie', tone: 'Sakin gün' },
   { src: '/photos/img-5299.jpg', alt: 'Gülüşler', tone: 'Kahkaha' },
   { src: '/photos/img-4972.jpg', alt: 'Şehir manzarasında çift', tone: 'Manzara' },
-  { src: '/photos/whatsapp-2.jpg', alt: 'Sıcak ve içten bir gülüş', tone: 'Samimiyet' },
+  { src: '/photos/img-20221115-205820-068.jpg', alt: 'Mutlu ve huzurlu bir an', tone: 'Sevgi' },
 ];
 
 const HERO_PHOTO = '/photos/img-7137.jpg';
